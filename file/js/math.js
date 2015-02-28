5bad5dcf0a7b9c5d0783d8fef2ce2b820b157d1d
riyue <sheng.yonggeng@gmail.com>
2015-02-28 11:26:01 +0800

    new add

---
+++ file/js/math.js
@@ -0,0 +1,34 @@
+var FMath={
+//对数
+logarithm:function(a,b){
+return Math.log(a)/Math.log(b);
+},
+//角度求弧度
+radian:function(a){
+return a*Math.PI/180;
+},
+//弧度求角度
+degree:function(){
+return a*180/Math.PI;
+},
+
+
+
+
+
+
+
+}
+/*
+//弧度
+//周长   2*PI*r = 2*180* 每度对应的圆弧的长度
+// 半径为1的圆对应的弧长就是 = PI/180
+// 如果弧长=r。那么这个圆心角称为1弧度
+*/
+
+
+
+
+
+
+
